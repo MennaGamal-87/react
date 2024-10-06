bff6d4c884ff0b11f49438f5407577d734358157
MennaGamal-87 <gmenna665@gmail.com>
2024-10-06 21:21:07 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+jQuery( document ).ready( function( $ ) {
+    'use strict';
+
+	//// ---> Check issue element
+	jQuery.fn.exists = function() {
+	  return jQuery(this).length;
+	}
+	
+	if($('.homeV2SliderWrap').exists()){
+		var homeV2Slider = $('.homeV2SliderWrap').bxSlider({
+			mode:"fade",
+			auto:true,
+			speed:700,
+			pause:4000,
+			onSlideAfter: function(){
+				var gcs = homeV2Slider.getCurrentSlide();
+				$('.homeV2SliderWrap').find('.homeV2SliderItem[data-slide="'+gcs+'"]').find(".homeV1PostDesc").each(function(){
+					var contentHeight = $(this).height();
+					contentHeight = (contentHeight * -1) / 2;
+					$(this).css("margin-top",contentHeight);
+				});
+		        $('.homeV2SliderWrap').find('.homeV2SliderItem:not(li[data-slide="'+gcs+'"])').removeClass("active");
+				$('.homeV2SliderWrap').find('.homeV2SliderItem[data-slide="'+gcs+'"]').addClass("active");
+		    }
+		});
+	}
+
+	if($('.twitterWidgetSlider').exists()){
+		var twitterWidgetSlider = $('.twitterWidgetSlider').bxSlider({
+			mode:"fade",
+			controls: false,
+			auto:true,
+			speed:500,
+			pause:3000
+		});
+	}
+
+	// $(".searchIcon").on("click", function(){
+	// 	$(".searchPopup").addClass("show").find('input[type="text"]').focus();
+	// 	alert('kndncjdn')
+	// });
+
+	$(document).on("click", ".searchIcon", function(){
+        console.log("Search icon clicked");
+        $(".searchPopup").addClass("show").find('input[type="text"]').focus();
+    });
+
+	$(document).on("click",".closeBtn", function(){
+		$(this).closest(".searchPopup").removeClass("show");
+	});
+
+	$(".homeV1PostDesc, .relatedPostDesc").each(function(){
+		var contentHeight = $(this).height();
+		contentHeight = (contentHeight * -1) / 2;
+		$(this).css("margin-top",contentHeight);
+	});
+
+	$(window).resize( function(e) {
+		$(".homeV1PostDesc, .relatedPostDesc").each(function(){
+			var contentHeight = $(this).height();
+			contentHeight = (contentHeight * -1) / 2;
+			$(this).css("margin-top",contentHeight);
+		});
+	});
+
+	$(document).on("click",'.showMobileMenu', function(e){
+		e.preventDefault();
+		$('.showMobileMenu').toggleClass('open').closest("body").toggleClass('animated');
+	});
+
+	$(window).load(function() {
+        if($('.masonryBox').exists()){
+			$(".masonryBox").masonry({
+				gutter: 30,
+				itemSelector: '.blogPostItem'
+			});	
+		}
+		$(".masonryBox").addClass("show");
+    });
+
+});
